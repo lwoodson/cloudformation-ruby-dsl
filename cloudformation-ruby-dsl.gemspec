08c885511051cb738620d1cbfc34a14d65b5eaaa
Lance Woodson <lance@webmaneuvers.com>
2016-08-24 09:25:42 -0500

    Setup basic acceptance tests
    Sets up a single acceptance test for the most basic
    case (cloudformation template that creates a single AWS bucket).
    Uses Rspec for testing.  Opts to validate the JSON output of the
    ruby DSL using aws cli tools to avoid many/any dependencies on
    cloudformation-ruby-dsl's internal structure/code.  Contains:
    
    - ruby DSL file is in examples/simple_template.rb
    - spec of this file is in spec/validation_spec.rb
    - lots of helpers that might be reused in other specs

--- cloudformation-ruby-dsl.gemspec
+++ cloudformation-ruby-dsl.gemspec
@@ -41,4 +41,5 @@ Gem::Specification.new do |gem|
   gem.add_runtime_dependency    'rake'
 
   gem.add_development_dependency 'rspec'
+  gem.add_development_dependency 'pry'
 end
--- spec/validation_spec.rb
+++ spec/validation_spec.rb
@@ -1,5 +1,21 @@
+require 'spec_helper'
+
 describe "cloudformation-ruby-dsl" do
-  it "should pass" do
-    expect(true).to eql(true)
+  include CommandHelpers
+  include JsonHelpers
+  include FileHelpers
+  include AwsHelpers
+
+  context "simplest template" do
+    let(:example_template) {"simple_template.rb"}
+
+    it "should create a valid JSON template from the example ruby template" do
+      delete_test_file("simple_template.json");
+      json = exec_cmd("./#{example_template} expand", :within => "examples").first
+      write_test_file("simple_template.json", json)
+      validate_cfn_template("simple_template.json")
+    end
   end
+
+  # TODO validate examples/cloudformation-ruby-script.rb
 end
