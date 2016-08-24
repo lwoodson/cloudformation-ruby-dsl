a94427d9a497998b42dcdf992cb5e662cdd54f6c
Lance Woodson <lance@webmaneuvers.com>
2016-08-24 09:54:11 -0500

    Move template validations into shared example group for reuse

--- spec/validation_spec.rb
+++ spec/validation_spec.rb
@@ -1,20 +1,25 @@
 require 'spec_helper'
 
-describe "cloudformation-ruby-dsl" do
+RSpec.shared_examples "template acceptance validations" do
   include CommandHelpers
   include JsonHelpers
   include FileHelpers
   include AwsHelpers
 
-  context "simplest template" do
-    let(:example_template) {"simple_template.rb"}
+  it "should create a valid JSON template from the example ruby template" do
+    delete_test_file(json_template)
+    json = exec_cmd("./#{ruby_template} expand", :within => "examples").first
+    write_test_file(json_template, json)
+    validate_cfn_template(json_template)
+  end
+end
 
-    it "should create a valid JSON template from the example ruby template" do
-      delete_test_file("simple_template.json");
-      json = exec_cmd("./#{example_template} expand", :within => "examples").first
-      write_test_file("simple_template.json", json)
-      validate_cfn_template("simple_template.json")
-    end
+describe "cloudformation-ruby-dsl" do
+  context "simplest template" do
+    let(:ruby_template) { "simple_template.rb" }
+    let(:json_template) { "simple_template.json" }
+
+    include_examples "template acceptance validations"
   end
 
   # TODO validate examples/cloudformation-ruby-script.rb
